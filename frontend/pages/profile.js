6da9ec232a4017f6f04c9d6397fb6e0d627a9342
ss251 <ss251@uw.edu>
2022-05-26 03:22:58 -0500

    able to mint successfully

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import AirEx from "../utils/AirEx.json";
-import { useMoralis, useWeb3ExecuteFunction } from "react-moralis";
+import {
+  useMoralis,
+  useWeb3ExecuteFunction,
+  useMoralisWeb3Api,
+} from "react-moralis";
+import LoadSpinner from "../components/LoadSpinner";
 import Moralis from "moralis";
 const ethers = Moralis.web3Library;
 import truncateEthAddress from "truncate-eth-address";
@@ -11,23 +16,97 @@ export default function Profile() {
     trait_type: "",
     trait_value: "",
   });
-  const { isAuthenticated, user } = useMoralis();
-  const { ethereum } = window;
-  const ABI = AirEx.abi;
-  const bytecode = AirEx.bytecode;
-  const provider = new ethers.providers.Web3Provider(ethereum);
-  const signer = provider.getSigner();
-  const contract = new ethers.Contract(
-    "0x7043390b0d6f39614133aB82e8f49574A177E56e",
-    ABI,
-    signer
-  );
-  const deployed = contract.attach(user.attributes.contractsCreated[0]);
+  const [minting, setMinting] = useState(null);
+  const [NFTs, setNFT] = useState({});
+  const Web3Api = useMoralisWeb3Api();
+  const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+  const { isAuthenticated, user, account } = useMoralis();
+
+  const NFTMetadata = {};
 
   const contractProcessor = useWeb3ExecuteFunction();
-  console.log(deployed);
 
-  async function mint() {}
+  const fetchAllTokenIds = async () => {
+    if (isAuthenticated) {
+      const options = {
+        address: account,
+        chain: "mumbai",
+      };
+      NFTMetadata = await Web3Api.account.getNFTs(options);
+      Object.keys(NFTMetadata).forEach((key) => {
+        if (key === "result") {
+          NFTMetadata = NFTMetadata[key];
+          setNFT(NFTMetadata);
+        }
+      });
+    }
+  };
+
+  async function mint() {
+    let requestAIREXOptions = {
+      abi: [
+        {
+          inputs: [
+            {
+              internalType: "string",
+              name: "_traitType",
+              type: "string",
+            },
+            {
+              internalType: "string",
+              name: "_traitValue",
+              type: "string",
+            },
+          ],
+          name: "requestAIREx",
+          outputs: [
+            {
+              internalType: "uint256",
+              name: "requestId",
+              type: "uint256",
+            },
+          ],
+          stateMutability: "payable",
+          type: "function",
+        },
+      ],
+      contractAddress: formInput.address,
+      functionName: "requestAIREx",
+      params: {
+        _traitType: formInput.trait_type,
+        _traitValue: formInput.trait_value,
+      },
+      value: 0,
+    };
+
+    let id = await contractProcessor.fetch({
+      params: requestAIREXOptions,
+    });
+    setMinting(true);
+
+    await delay(50000);
+
+    let revealAIREXOptions = {
+      abi: [
+        {
+          inputs: [],
+          name: "revealAIREx",
+          outputs: [],
+          stateMutability: "nonpayable",
+          type: "function",
+        },
+      ],
+      contractAddress: formInput.address,
+      functionName: "revealAIREx",
+      params: {},
+    };
+
+    let tx = await contractProcessor.fetch({
+      params: revealAIREXOptions,
+    });
+    await delay(10000);
+    setMinting(false);
+  }
   return (
     <div>
       {isAuthenticated ? (
@@ -72,17 +151,23 @@ export default function Profile() {
             <h4 className="mt-8 font-bold">TRAIT VALUE</h4>
             <h4 className="w-140 mt-2 mb-2">Specify a trait value string</h4>
             <input
-              className="mt-2 border-blue-500 rounded p-4 w-80 bg-blue-form-field mb-14"
+              className="mt-2 border-blue-500 rounded p-4 w-80 bg-blue-form-field"
               onChange={(e) => {
                 updateFormInput({ ...formInput, trait_value: e.target.value });
               }}
             ></input>
-            <button
-              onClick={mint}
-              className="bg-gradient-to-r from-teal-400 to-blue-500 hover:from-pink-600 hover:to-orange-600 text-white font-semibold px-4 py-2 rounded w-40 mb-20 mt-20 lg:mt-0"
-            >
-              Mint NFT
-            </button>
+            {!minting ? (
+              <button
+                onClick={mint}
+                className="bg-gradient-to-r from-teal-400 to-blue-500 hover:from-pink-600 hover:to-orange-600 text-white font-semibold px-4 py-2 mb-20 rounded w-40 mt-14 lg:mt-14"
+              >
+                Mint NFT
+              </button>
+            ) : (
+              <div className="-mt-24 mb-14">
+                <LoadSpinner />
+              </div>
+            )}
           </div>
         </div>
       ) : (
