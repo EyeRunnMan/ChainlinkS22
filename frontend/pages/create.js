7fcb762d4552288077fe2e3cb66f3ee67081c9bb
ss251 <ss251@uw.edu>
2022-05-25 20:59:03 -0500

    save collections to moralis DB

--- frontend/pages/create.js
+++ frontend/pages/create.js
@@ -2,15 +2,16 @@ import React, { useState } from "react";
 // import { create as ipfsHttpClient } from "ipfs-http-client";
 import { Web3Storage } from "web3.storage";
 import Moralis from "moralis";
-import { useMoralis } from "react-moralis";
+import { useMoralis, useMoralisQuery } from "react-moralis";
 import AirEx from "../utils/AirEx.json";
+import { useRouter } from "next/router";
 const ethers = Moralis.web3Library;
 
 //const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
 const client = new Web3Storage({ token: process.env.NEXT_PUBLIC_WEB3_API_KEY });
 
 export default function Create() {
-  const { isAuthenticated } = useMoralis();
+  const { isAuthenticated, user } = useMoralis();
   const [fileUrl, setFileUrl] = useState(null);
   const [formInput, updateFormInput] = useState({
     name: "",
@@ -20,6 +21,9 @@ export default function Create() {
     accessLink: "",
     subscriptionId: 137,
   });
+  const router = useRouter();
+
+  const User = Moralis.Object.extend("_User");
 
   async function onSubmit(e) {
     const { ethereum } = window;
@@ -57,10 +61,21 @@ export default function Create() {
         formInput.subscriptionId,
       ]);
       console.log(contract.address, contract.deployTransaction);
+      if (user.attributes.contractsCreated === undefined) {
+        user.save([], {
+          onSuccess: (user) => {
+            user.set("contractsCreated", []);
+          },
+        });
+      }
+      user.addUnique("contractsCreated", contract.address);
+      user.save();
     }
+    router.push("/");
   }
 
   async function onChange(e) {
+    console.log(user);
     if (e.target.files.length === 1) {
       //const file = e.target.files[0];
 
@@ -117,66 +132,76 @@ export default function Create() {
 
   return (
     <div>
-      <div className="w-1/4 ml-60 flex flex-col pb-12">
-        <h1 className="mt-20 font-bold text-3xl">CREATE NEW ITEM</h1>
-        <h4 className="mt-12 font-bold">IMAGE, VIDEO, AUDIO, OR 3D MODEL</h4>
-        <h4 className="w-140 mt-2">
-          File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV, OGG,
-          GLB, GLTF. Max size: 100 MB
-        </h4>
-        <input
-          type="file"
-          placeholder="Asset"
-          className="my-4"
-          onChange={onChange}
-        />
-        {!fileUrl && <img src="/uploadimg.png" />}
-        {fileUrl && <img className="rounded mt-4" width="350" src={fileUrl} />}
-        <h4 className="mt-8 font-bold">NAME</h4>
-        <input
-          className="mt-2 border-blue-500 rounded p-4 bg-blue-form-field"
-          onChange={(e) => {
-            updateFormInput({ ...formInput, name: e.target.value });
-          }}
-        ></input>
-        <h4 className="mt-8 font-bold">SYMBOL</h4>
-        <input
-          className="mt-2 border-blue-500 rounded p-4 bg-blue-form-field"
-          onChange={(e) => {
-            updateFormInput({ ...formInput, symbol: e.target.value });
-          }}
-        ></input>
-        <h4 className="mt-8 font-bold">EXTERNAL LINK</h4>
-        <h4 className="w-140 mt-2 mb-2">
-          AIRNFT will include a link to this URL on this item's detail page, so
-          that users can click to learn more about it. You are welcome to link
-          to your own webpage with more details.
-        </h4>
-        <input
-          directory=""
-          webkitdirectory=""
-          type="file"
-          onChange={onChange}
-        />
-        <h4 className="mt-8 font-bold">DESCRIPTION</h4>
-        <h4 className="w-140 mt-2">
-          The description will be included on the item's detail page underneath
-          its image.
-        </h4>
-        <input
-          className="mt-4 border rounded p-4 bg-blue-form-field"
-          onChange={(e) => {
-            updateFormInput({ ...formInput, description: e.target.value });
-          }}
-        ></input>
+      {isAuthenticated ? (
+        <div className="lg:w-1/4 w-1/2 lg:ml-60 ml-24 -mt-8 lg:mt-0 flex flex-col pb-12">
+          <h1 className="mt-20 font-bold text-3xl">CREATE NEW COLLECTION</h1>
+          <h4 className="mt-12 font-bold">IMAGE, VIDEO, AUDIO, OR 3D MODEL</h4>
+          <h4 className="w-140 mt-2 text-blue-500">
+            File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV, OGG,
+            GLB, GLTF.
+          </h4>
+          <input
+            type="file"
+            placeholder="Asset"
+            className="my-4"
+            onChange={onChange}
+          />
+          {!fileUrl && <img src="/uploadimg.png" />}
+          {fileUrl && (
+            <img className="rounded mt-4" width="350" src={fileUrl} />
+          )}
+          <h4 className="mt-8 font-bold">NAME</h4>
+          <input
+            className="mt-2 border-blue-500 rounded p-4 bg-blue-form-field"
+            onChange={(e) => {
+              updateFormInput({ ...formInput, name: e.target.value });
+            }}
+          ></input>
+          <h4 className="mt-8 font-bold">SYMBOL</h4>
+          <input
+            className="mt-2 border-blue-500 rounded p-4 bg-blue-form-field"
+            onChange={(e) => {
+              updateFormInput({ ...formInput, symbol: e.target.value });
+            }}
+          ></input>
+          <h4 className="mt-8 font-bold">EXTERNAL LINK</h4>
+          <h4 className="w-140 mt-2 mb-2">
+            Upload your game folder that will be used to deploy a new instance
+            of the AIRNFT smart contract, letting you mint randomized NFTs from
+            this collection.
+          </h4>
+          <input
+            directory=""
+            webkitdirectory=""
+            type="file"
+            onChange={onChange}
+          />
+          <h4 className="mt-8 font-bold">DESCRIPTION</h4>
+          <h4 className="w-140 mt-2">
+            The description will be included on the item's detail page
+            underneath its image.
+          </h4>
+          <input
+            className="mt-4 border rounded p-4 bg-blue-form-field"
+            onChange={(e) => {
+              updateFormInput({ ...formInput, description: e.target.value });
+            }}
+          ></input>
 
-        <button
-          className="font-bold mt-12 mb-24 w-48 bg-blue-form-button rounded p-4 shadow-lg"
-          onClick={onSubmit}
-        >
-          CREATE
-        </button>
-      </div>
+          <button
+            className="font-bold mt-12 mb-24 lg:w-48 sm:w-24 bg-gradient-to-r from-teal-400 to-blue-500 hover:from-pink-600 hover:to-orange-600 text-white rounded p-4 shadow-lg"
+            onClick={onSubmit}
+          >
+            CREATE
+          </button>
+        </div>
+      ) : (
+        <div className="h-screen">
+          <p className="ml-[30%] lg:ml-[40%] mt-20 text-xl lg:text-3xl">
+            Please connect your wallet
+          </p>
+        </div>
+      )}
     </div>
   );
 }
